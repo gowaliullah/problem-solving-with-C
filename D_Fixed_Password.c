b62173caf9d5c6bafd52e9e4d70ec74787871d20
MD Waliullah <waliullah.godev@gmail.com>
2025-10-14 15:05:22 +0600

    with for loop

--- D_Fixed_Password.c
+++ D_Fixed_Password.c
@@ -14,6 +14,19 @@ int main() {
          printf("Wrong\n");
        }
     }
+
+    // with for loop
+
+      for (;scanf("%d", &pass) != EOF;)
+    {
+       if (pass == 1999)
+       {
+        printf("Correct");
+        break;
+       } else {
+         printf("Wrong\n");
+       }
+    }
     
     
     return 0;
