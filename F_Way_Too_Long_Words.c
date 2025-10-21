8117d804b7c5faa39a9397e56e6591ac6bedd665
MD Waliullah <waliullah.godev@gmail.com>
2025-10-21 11:31:17 +0600

    Way Too Long Words

--- F_Way_Too_Long_Words.c
+++ F_Way_Too_Long_Words.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
-int main() {
-    
-    
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    char s[n][101];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%s", s[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int len = strlen(s[i]);
+
+        if (len <= 10)
+        {
+            printf("%s\n", s[i]);
+        }
+        else
+        {
+            printf("%c%d%c\n", s[i][0], len - 2, s[i][len - 1]);
+        }
+    }
+
     return 0;
 }
