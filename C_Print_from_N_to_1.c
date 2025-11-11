afd35c69d0d33d3ca0c189d5c6d058b3724d6a6e
MD Waliullah <waliullah.godev@gmail.com>
2025-11-11 11:11:05 +0600

    pritn N to 1 with space based on logic by using recursing

---
+++ C_Print_from_N_to_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void print(int n, int i)
+{
+    if (i != n + 1)
+    {
+        print(n, i + 1);
+        if (i != 1)
+        {
+            printf("%d ", i);
+        }
+        else
+        {
+            printf("%d", i);
+        }
+    }
+}
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    print(n, 1);
+
+    return 0;
+}
