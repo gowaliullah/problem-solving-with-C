71fcd157eebcd8444ae0438d65c50819604bb7fc
MD Waliullah <waliullah.godev@gmail.com>
2025-11-14 21:01:11 +0600

    magic tree

--- Final Exam/Magical_tree.c
+++ Final Exam/Magical_tree.c
@@ -23,22 +23,26 @@ int main()
     case 9:
         n = 10;
          break;
+    case 11:
+        n = 11;
+         break;
+    case 13:
+        n = 12;
+         break;
+    case 15:
+        n = 13;
+         break;
+    case 17:
+        n = 14;
+         break;
+    case 19:
+        n = 15;
+         break;
+    case 21:
+        n = 16;
+         break;
     }
 
-    /*
-
-    if (n == 1)
-    {
-        n = 6;
-    }
-    else if (n == 3)
-    {
-        n = 7;
-    } else if (n == 5)
-    {
-        n = 8;
-    }
-    */
 
     int str = 1;
     int spc = n - 1;
@@ -63,28 +67,61 @@ int main()
 
     for (int i = 0; i < 5; i++)
     {
-
-        for (int j = n; j > 1; j--)
+        for (int j = 6; j > 1; j--)
         {
             printf(" ");
         }
 
         for (int k = 0; k < 1; k++)
         {
-            if (n = 6)
+            if (n == 6)
             {
                 printf("*");
             }
-            else if (n = 7)
+            else if (n == 7)
             {
                 printf("***");
+            }
+            else if (n == 8)
+            {
+                printf("*****");
+            }
+            else if (n == 9)
+            {
+                printf("*******");
+            }
+            else if (n == 10)
+            {
+                printf("*********");
+            }
+            else if (n == 11)
+            {
+                printf("***********");
+            }
+            else if (n == 12)
+            {
+                printf("*************");
+            }
+            else if (n == 13)
+            {
+                printf("***************");
+            }
+            else if (n == 14)
+            {
+                printf("*****************");
+            }
+            else if (n == 15)
+            {
+                printf("*******************");
+            }
+            else if (n == 16)
+            {
+                printf("*********************");
             }
         }
 
         printf("\n");
     }
-
-    printf("%d", n);
 
     return 0;
 }
